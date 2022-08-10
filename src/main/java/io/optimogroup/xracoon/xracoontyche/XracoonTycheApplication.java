ae4a157361b1133f279d45e7188637b0c4b0b81b
shako <davitashvilishako1@gmail.com>
2022-08-10 18:04:04 +0400

    Initial commit

---
+++ src/main/java/io/optimogroup/xracoon/xracoontyche/XracoonTycheApplication.java
@@ -0,0 +1,13 @@
+package io.optimogroup.xracoon.xracoontyche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XracoonTycheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XracoonTycheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/optimogroup/xracoon/xracoontyche/XracoonTycheApplicationTests.java
@@ -0,0 +1,13 @@
+package io.optimogroup.xracoon.xracoontyche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XracoonTycheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
